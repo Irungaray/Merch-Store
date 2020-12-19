a44e2b79a4cbefc511eba4bdd725dae662a96535
Irungaray <lionelirungaray@gmail.com>
2020-12-19 08:44:58 -0300

    Info Form

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,7 +1,52 @@
 import React from 'react';
 
+import '../styles/components/Information.css';
+
 const Information = () => {
-  return <h1>Information</h1>;
-};
+  return (
+    <div className="Information">
+      <div className="Information-content">
+        <div className="Information-head">
+          <h2>Contact Information:</h2>
+        </div>
+
+        <div className="Information-form">
+          <form action="">
+            <input type="text" placeholder="Name" name="name" />
+            <input type="text" placeholder="Email" name="email" />
+            <input type="text" placeholder="Country" name="country" />
+            <input type="text" placeholder="City" name="city" />
+            <input type="text" placeholder="State" name="state" />
+            <input type="text" placeholder="Adress" name="adress" />
+            <input type="text" placeholder="Apartament" name="apto" />
+            <input type="text" placeholder="Postal code" name="cp" />
+            <input type="text" placeholder="Phone Number" name="phone" />
+          </form>
+        </div>
+
+        <div className="Information-buttons">
+          <div className="Information-back">
+            Go Back
+          </div>
+
+          <div className="Information-next">
+            Continue and Pay
+          </div>
+        </div>
+      </div>
+
+      <div className="information-Sidebar">
+        <h3>Order:</h3>
+
+        <div className="Information-item">
+          <div className="Information-element">
+            <h4>ITEM Name</h4>
+            <span>$10</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Information;
