59fa753aa8c145d3a94c6ac25d555f4d6a0e143a
Irungaray <lionelirungaray@gmail.com>
2020-12-23 07:00:49 -0300

    Contexted cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+
+import AppContext from '../context/AppContext';
 
 import '../styles/components/Header.css';
 
 const Header = () => {
+  const { state } = useContext(AppContext);
+  const { cart } = state;
+
   return (
     <div className="Header">
       <h1 className="Header-title">
@@ -14,6 +19,9 @@ const Header = () => {
         <Link to="/checkout">
           <i className="fas fa-shopping-basket">Checkout</i>
         </Link>
+
+        {cart.length > 0
+          && <div className="Header-alert">{cart.length}</div>}
       </div>
     </div>
   );
