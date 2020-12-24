361261c007ff1c2ea310a77871e776814841697e
Irungaray <lionelirungaray@gmail.com>
2020-12-24 15:15:16 -0300

    DotEnved

---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+const config = {
+  clientIdEnv: String(process.env.CLIENT_ID)
+}
+
+export default config;
--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { PayPalButton } from 'react-paypal-button';
 
 import AppContext from '../context/AppContext';
-import handleSumTotal from '../utils/index'
+import handleSumTotal from '../utils'
 
 import '../styles/components/Payment.css';
 
@@ -10,14 +10,14 @@ const Payment = ({ history }) => {
   const { state, addNewOrder } = useContext(AppContext);
   const { cart, buyer } = state;
 
+
   const paypalOptions = {
-    clientId:
-      'AYOgBQAQ2-58eRZCiqN4sdFwx6rWWrP-D-FX6Zz-S7lbqmFiidz8MdWCwVGDAFzr0ARfNKsQIWwYJekC',
+    clientId: process.env.REACT_APP_CLIENT_ID,
     intent: 'capture',
     currency: 'USD',
   };
 
-  const buttonSyles = {
+  const buttonStyles = {
     layout: 'vertical',
     shape: 'rect',
   };
@@ -33,7 +33,7 @@ const Payment = ({ history }) => {
       };
 
       addNewOrder(newOrder);
-      history.push('/checkout/succes');
+      history.push('/checkout/success');
     }
   };
 
@@ -53,20 +53,12 @@ const Payment = ({ history }) => {
         <div className="Payment-button">
           <PayPalButton
             paypalOptions={paypalOptions}
-            buttonStyles={buttonSyles}
+            buttonStyles={buttonStyles}
             amount={handleSumTotal(cart)}
-            onPaymentStart={() =>
-              console.log('Payment Started')
-            }
-            onPaymentSuccess={(data) =>
-              handlePaymentSuccess('Payment Success:', data)
-            }
-            onPaymentError={(error) =>
-              console.log('Payment error:', error)
-            }
-            onPaymentCancel={(data) =>
-              console.log('Payment canceled:', data)
-            }
+            onPaymentStart={() => console.log('Start Payment')}
+            onPaymentSuccess={data => handlePaymentSuccess(data)}
+            onPaymentError={error => console.log(error)}
+            onPaymentCancel={data => console.log(data)}
           />
         </div>
       </div>
