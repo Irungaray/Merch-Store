c738062451ad3649a4fee50464941463898b61f7
Irungaray <lionelirungaray@gmail.com>
2020-12-25 08:34:58 -0300

    Converted to PWA

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ import '../styles/components/Footer.css';
 const Footer = () => {
   return (
     <div className="Footer">
-      <p className="Footer-title">Merch</p>
-      <p className="Footer-copy">Copiame esta</p>
+      <p className="Footer-title">Merch Store</p>
+      <p className="Footer-copy">All Rights Reserved</p>
     </div>
   );
 };
